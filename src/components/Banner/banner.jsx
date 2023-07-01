75d40fcce3873062bb40b4b382cd0b3d747725b1
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-07-01 19:21:12 +0200

    Added some SEO optimizations

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -59,6 +59,7 @@ export default function Banner() {
   };
   return (
     <div className="banner-container">
+      <h1 style={{"fontSize": "0px"}}>Jose María Campos Desarrollador Web</h1>
       <div className="banner-row-center">
         <div style={{ flex: 1 }}>
           <AnimatedLetters title={"Hola ,"} />
